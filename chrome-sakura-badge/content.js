203c3a883a18a4ea1cfbc23d555795da48d5f13b
kamijin_fanta <kamijin@live.jp>
2016-04-10 12:55:52 +0900

    使用されていないコードの削除。オプションページにGithubへのリンクを追加。

--- chrome-sakura-badge/content.js
+++ chrome-sakura-badge/content.js
@@ -3,10 +3,3 @@ var loadTimes = window.chrome.loadTimes();
 chrome.runtime.sendMessage({
     location: window.location
 });
-
-chrome.tabs.onActivated.addListener(function (tabId) {
-    console.log("select change");
-    chrome.runtime.sendMessage({
-        location: window.location
-    });
-});
