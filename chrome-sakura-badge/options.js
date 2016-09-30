2632089a88a53d0b1d71031ca6aabc9573e5bff6
kamijin-fanta <kamijin@live.jp>
2016-09-30 10:48:45 +0900

    オプションページにドメインの表示履歴を追加

--- chrome-sakura-badge/options.js
+++ chrome-sakura-badge/options.js
@@ -4,3 +4,21 @@ function cacheClear(){
     alert(`${len}件のキャッシュを削除しました`);
 }
 document.getElementById('cacheClear').addEventListener('click', cacheClear);
+
+(function() {
+    var table = document.getElementById("cached");
+    for(var name in localStorage) {
+        var nameTd = document.createElement("td");
+        nameTd.textContent = name;
+
+        var asTd = document.createElement("td");
+        var asn = JSON.parse(localStorage.getItem(name)).asn;
+        asTd.textContent = asn?asn:"×";
+
+        var tr = document.createElement("tr");
+        tr.appendChild(nameTd);
+        tr.appendChild(asTd);
+
+        table.appendChild(tr);
+    }
+})();
